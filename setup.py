c94ec9cdb9c58f0137c1dfbbde3f15a29f238a55
martinkosir <martin@martinkosir.net>
2016-10-31 11:38:30 +0100

    Removed Python 2.6 from supported versions.

--- setup.py
+++ setup.py
@@ -36,7 +36,6 @@ setup(
         'Topic :: Communications :: Email',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
